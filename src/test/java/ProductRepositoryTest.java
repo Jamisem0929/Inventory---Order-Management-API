3562c33b7ca6e0dbd53d56cf9a2d5928db3f0bca
jamisem0929 <ethan.jamison@washburn.edu>
2026-09-11 17:02:56 -0500

    Removed unused imports

--- src/test/java/ProductRepositoryTest.java
+++ src/test/java/ProductRepositoryTest.java
@@ -1,14 +1,10 @@
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class ProductRepositoryTest {
     public static void main(String[] args) throws SQLException {
-        
-    
-        
+
+
+
         ProductRepository productRepository = new ProductRepository();
         productRepository.deleteProduct(3);
         productRepository.getProductById(3);
