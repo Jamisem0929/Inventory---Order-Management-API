29a7fb46d109cf1198972eaf29510a6459763717
jamisem0929 <ethan.jamison@washburn.edu>
2026-09-11 17:01:27 -0500

    compiled the program

--- src/test/java/CustomerRepositoryTest.java
+++ src/test/java/CustomerRepositoryTest.java
@@ -3,7 +3,6 @@ import java.sql.SQLException;
 public class CustomerRepositoryTest {
     public static void main(String[] args) throws SQLException{
         CustomerRepository repository = new CustomerRepository();
-        
        repository.deleteCustomer(2);
     }
 }
